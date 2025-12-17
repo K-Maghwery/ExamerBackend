f327d6f4460f95d83b01ef1365d18408922d0de8
habiba03 <habibamohammedyahia@gmail.com>
2025-12-17 16:48:15 +0200

    hhhh

---
+++ examer/src/main/java/com/online_exam/examer/exam_submission/dto/ExamQuestionsDto.java
@@ -0,0 +1,23 @@
+package com.online_exam.examer.exam_submission.dto;
+
+import com.online_exam.examer.question.enums.QuestionType;
+import com.online_exam.examer.question.dto.OptionDto;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+public class ExamQuestionsDto implements Serializable {
+
+    private Long questionId;
+
+    private String questionContent;
+
+    private QuestionType questionType;
+
+    // Null for WRITTEN
+    private List<OptionDto> options;
+
+    private List<Integer> correctOptionIndexes;
+}
