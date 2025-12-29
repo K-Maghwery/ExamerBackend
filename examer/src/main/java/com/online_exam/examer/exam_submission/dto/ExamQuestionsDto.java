943acb927e93856736046d4f4af4f38e661f85e6
habiba03 <116196717+habiba03@users.noreply.github.com>
2025-12-29 14:28:44 +0200

    Habiba (#3)
    * hhhh
    
    * update question response
    
    * update question response
    
    * save questions

--- examer/src/main/java/com/online_exam/examer/exam_submission/dto/ExamQuestionsDto.java
+++ examer/src/main/java/com/online_exam/examer/exam_submission/dto/ExamQuestionsDto.java
@@ -1,5 +1,6 @@
 package com.online_exam.examer.exam_submission.dto;
 
+import com.online_exam.examer.question.QuestionOptionEntity;
 import com.online_exam.examer.question.enums.QuestionType;
 import com.online_exam.examer.question.dto.OptionDto;
 import lombok.Data;
@@ -17,7 +18,8 @@ public class ExamQuestionsDto implements Serializable {
     private QuestionType questionType;
 
     // Null for WRITTEN
-    private List<OptionDto> options;
+//    private List<OptionDto> options;
+    private List<String> options;
 
     private List<Integer> correctOptionIndexes;
 }
